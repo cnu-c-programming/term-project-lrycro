274a20d628ff5cf54a290b8a1a092d18b0e3a14f
lrycro <thscw973@naver.com>
2026-05-18 22:03:53 +0900

    feat: student.h, student.c 작성 (테스트 전)

---
+++ student.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
+
+// 1. 학생 추가
+Student* add_student(Student* head, int id, const char* name, int score) {
+	Student* new_student = (Student*)malloc(sizeof(Student));
+	if (new_student == NULL) {
+		fprintf(stderr, "Error: Memory Allocation Failed.\n");
+		return head;
+	}
+	
+	new_student->id = id;
+	strncpy(new_student->name, name, sizeof(new_student->name) - 1);
+	new_student->name[sizeof(new_student->name) - 1] = '\0';
+	new_student->score = score;
+	new_student->next = NULL;
+
+	if (head == NULL) {
+		return new_student;
+	}
+
+	Student* curr = head;
+	
+	while (curr->next != NULL) {
+		curr = curr->next;
+	}
+	curr->next = new_student;
+
+	return head;
+}
+
+// 2. 학생 삭제
+Student* delete_student(Student* head, int id) {
+	if (head == NULL) {
+		return NULL;
+	}
+
+	if (head->id == id) {
+		Student* tmp = head;
+		head = head->next;
+		free(tmp);
+		return head;
+	}
+
+	Student* curr = head;
+	
+	while (curr->next != NULL) {
+		if (curr->next->id == id) {
+			Student* tmp = curr->next;
+			curr->next = tmp->next;
+			free(tmp);
+			return head;
+		}
+		curr = curr->next;
+	}
+	return head;
+}
+
+Student* find_student(Student* head, int id) {
+	Student* curr = head;
+
+	while (curr != NULL) {
+		if (curr->id == id) {
+			return curr;
+		}
+		curr = curr->next;
+	}
+	return NULL;
+}
+
+void list_studnets(Student* head) {
+	if (head == NULL) {
+		// TC07
+		printf("No students found.\n");
+		return;
+	}
+
+	printf("ID\tname\t\tScore\n");
+
+	Student* curr = head;
+	
+	while (curr != NULL) {
+		printf("%d\t%s\t\t%d\n", curr->id, curr->name, curr->score);
+		curr = curr->next;
+	}
+}
+
+void free_students(Student* head) {
+	Student* curr = head;
+	Student* next2;
+
+	while (curr != NULL) {
+		next2 = curr->next;
+		free(curr);
+		curr = next2;
+	}
+}
---
+++ student.h
@@ -0,0 +1,18 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct Student {
+	int id;
+	char name[32];
+	int score;
+	struct Student* next;
+} Student;
+
+Student* add_student(Student* head, int id, const char* name, int score);
+Student* delete_student(Student* head, int id);
+Student* find_student(Student* head, int id);
+
+void list_students(Student* head);
+void free_students(Student* head);
+
+#endif // STUDENT_H
